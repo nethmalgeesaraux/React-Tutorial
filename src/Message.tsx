940e7d0cf076e7bd264213bf12daf3fd2d50fd48
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-11-19 22:01:47 +0530

    pascal-case

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//pascal-case
+function Massage() {
+    //JSX : javaScript XML
+    const name = "Nethmal";
+    if(name)
+    return <h1>Heloo {name}</h1>;
+    return <h1>Heloo Guest</h1>;
+}
+export default Massage;
