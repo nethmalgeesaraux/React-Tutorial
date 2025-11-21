4ab0829b65ca93e651a2383c45ef1b62280b456a
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-11-21 23:41:55 +0530

    Coditional Rendering

--- src/component/ListGroup.tsx
+++ src/component/ListGroup.tsx
@@ -1,31 +1,21 @@
 import { Fragment } from "react/jsx-runtime";
 import './ListGroup.css';
 function ListGroup() {
+
+    let items = ["nethmal", "malaka", "shan", "dilshan"];
+    items = []; 
+    
+    
+        
     return <div>
-        <h1 className="center-heading">List Group</h1>
-        <ul className="list-group list-group-horizontal">
-            <li className="list-group-item name-item">Name</li>
-            <li className="list-group-item age-item">Age</li>
-            <li className="list-group-item institute-item">Institute</li>
+        <h1>List Group</h1>
+        {items.length === 0 && <p>No Item Found</p>}
+        <ul className="list-group">
+            {items.map((items)=> (
+                <li key={items}>{items}</li>
+            ))}
         </ul>
-
-
-        <ul className="list-group list-group-horizontal-sm">
-            <li className="list-group-item">Malaka</li>
-            <li className="list-group-item">20</li>
-            <li className="list-group-item">Icet</li>
-        </ul>
-        <ul className="list-group list-group-horizontal-md">
-            <li className="list-group-item">Nethmal</li>
-            <li className="list-group-item">20</li>
-            <li className="list-group-item">Icet</li>
-        </ul>
-        <ul className="list-group list-group-horizontal-lg">
-            <li className="list-group-item">Shan</li>
-            <li className="list-group-item">23</li>
-            <li className="list-group-item">Icet</li>
-        </ul>
-
+       
     </div>
 }
 export default ListGroup;
