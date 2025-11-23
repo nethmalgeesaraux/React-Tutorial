a2cf6ee6f4a25ffe8295c27b6fe46f64eb8d8358
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-11-23 23:06:44 +0530

    Managing State

--- src/component/ListGroup.tsx
+++ src/component/ListGroup.tsx
@@ -1,18 +1,20 @@
 import { Fragment } from "react/jsx-runtime";
 import './ListGroup.css';
+import { useState } from "react";
 function ListGroup() {
 
     let items = ["nethmal", "malaka", "shan", "dilshan"];
+    const[selectedIndex, setSelectedIndex] = useState(-1);
     //items = []; 
     
-    const handleClick = (event: React.MouseEvent<HTMLLIElement>) => console.log(event);
+    //const handleClick = (event: React.MouseEvent<HTMLLIElement>) => console.log(event);
         
     return <div>
         <h1>List Group</h1>
         {items.length === 0 && <p>No Item Found</p>}
         <ul className="list-group">
-            {items.map((items)=> (
-                <li className="list-group-item" key={items} onClick={handleClick}>{items}</li>
+            {items.map((items,index)=> (
+                <li className={selectedIndex === index? "list-group-item active": "list-group-item"} key={items} onClick={()=> setSelectedIndex(index)}>{items}</li>
             ))}
         </ul>
        
