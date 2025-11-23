011bb1b4749e5256f2aebc61349ed57973d1fb6b
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-11-24 00:31:19 +0530

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Alert from "./component/Alert";
 import ListGroup from "./component/ListGroup";
 
 function App() {
 let items = ["nethmal", "malaka", "shan", "dilshan"];
 const onSelectItem = (item: string) => console.log(item);
 
-  return <div><ListGroup items={items} heading="List of names(Cities)"onSelectItem={onSelectItem}/></div>
+  return <div><ListGroup items={items} heading="List of names(Cities)"onSelectItem={onSelectItem}/>
+  <hr></hr>
+  <hr></hr>
+  <Alert>
+    Hello <span>World</span>
+  </Alert>
+  </div>
 }
 
 export default App;
